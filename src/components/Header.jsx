5bbdca91a9913eb998ee6def293eda04b01f63eb
raingsey72 <70264383+raingsey72@users.noreply.github.com>
2024-12-10 15:17:19 +0700

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export function Header(props) {
+    const { todos } = props
+    const todosLength = todos.length
+
+    const isTasksPlural = todos.length != 1
+    const tasksOrTasks = isTasksPlural ? 'tasks' : 'task'
+
+    return(
+        <header>
+            <h1 className="text-gradient">You have {todosLength} open {tasksOrTasks}.</h1>
+        </header>
+    )
+}
